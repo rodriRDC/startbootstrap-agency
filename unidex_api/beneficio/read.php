d7d3b41d63ba9e6efc401073b952334dab94d9a7
rodriRDC <rodrigo.d.contreras@gmail.com>
2019-09-19 19:03:28 -0300

    .

--- unidex_api/beneficio/read.php
+++ unidex_api/beneficio/read.php
@@ -5,34 +5,26 @@ header("Content-Type: application/json; charset=UTF-8");
  
 // include database and object files
 include_once '../config/database.php';
-//include_once '../objects/product.php';
 include_once '../objects/beneficio.php';
  
-// instantiate database and product object
+// instantiate database and beneficio object
 $database = new Database();
 $db = $database->getConnection();
  
 // initialize object
 $beneficio = new Beneficio($db);
  
-// query products
+// query beneficios
 $stmt = $beneficio->read();
 $num = $stmt->rowCount();
  
-// check if more than 0 record found
 if($num>0){
  
-    // products array
     $beneficios_arr=array();
     $beneficios_arr["records"]=array();
  
-    // retrieve our table contents
-    // fetch() is faster than fetchAll()
-    // http://stackoverflow.com/questions/2770630/pdofetchall-vs-pdofetch-in-a-loop
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-        // extract row
-        // this will make $row['name'] to
-        // just $name only
+
         extract($row);
  
         $product_item=array(
@@ -47,7 +39,7 @@ if($num>0){
     // set response code - 200 OK
     http_response_code(200);
  
-    // show products data in json format
+    // show beneficios data in json format
     echo json_encode($beneficios_arr);
 
 }
@@ -56,7 +48,7 @@ else{
     // set response code - 404 Not found
     http_response_code(404);
  
-    // tell the user no products found
+    // tell the user no beneficios found
     echo json_encode(
         array("message" => "No products found.")
     );

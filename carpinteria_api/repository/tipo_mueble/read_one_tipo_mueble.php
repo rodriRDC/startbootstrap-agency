add18252eeb82d12c25fa9b517d82d28bbbe00e1
rodriRDC <rodrigo.d.contreras@gmail.com>
2020-01-25 17:53:25 -0300

    Read one

--- carpinteria_api/repository/tipo_mueble/read_one_tipo_mueble.php
+++ carpinteria_api/repository/tipo_mueble/read_one_tipo_mueble.php
@@ -21,27 +21,10 @@ $tipoMueble = new Tipo_mueble_pdo($db);
 // set ID property of record to read
 $tipoMueble->id = isset($_GET['id']) ? $_GET['id'] : die();
 
-$tipoMueble->id = 1;
-$tipoMueble->descripcion = "sarasa";
-$tipoMueble->detalle = "detalle sarasa";
- 
-// read the details of product to be edited
-//$tipoMueble->readOne();
- 
-if($tipoMueble->descripcion!=null){
- 
-    // set response code - 200 OK
-    http_response_code(200);
- 
-    // make it json format
-    echo json_encode($tipoMueble);
-}
- 
-else{
-    // set response code - 404 Not found
-    http_response_code(404);
- 
-    // tell the user product does not exist
-    echo json_encode(array("message" => "Product does not exist."));
-}
+$tipoMueble->readOne();
+
+http_response_code(200);
+
+echo json_encode($tipoMueble);
+
 ?>
